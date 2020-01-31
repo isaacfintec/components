50eec955a42a8bdaf43f60929ed9c3c4f46cb92e
Isaac Navarro <isaac@fintecimal.com>
2020-01-30 18:04:36 -0600

    .

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import 'animate.css';
 import './index.css';
-import App from './App';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
